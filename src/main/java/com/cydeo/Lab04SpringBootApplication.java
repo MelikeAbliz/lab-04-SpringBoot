3c38e93827ebc88c8971f0190b9ab0a381dad3a2
MelikeAbliz <medine20171121@gmail.com>
2024-04-07 21:21:53 +0300

    first commit

---
+++ src/main/java/com/cydeo/Lab04SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringBootApplication.class, args);
+    }
+
+}
